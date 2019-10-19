6a6618d3a82037dccf35a225ef011199fa7a1ab0
Phelo Macanda <phelo.macanda@wyzetalk.com>
2019-10-19 03:09:12 +0200

    We're now able to call api's from port 3100

--- eazi-front/src/Api/App.js
+++ eazi-front/src/Api/App.js
@@ -2,3 +2,31 @@
 // Phelo Macanda
 // 19 October 2019
 
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+// const router = express.Router();
+
+
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/candidate-login', (req, res, next) => {
+    res.send('<form action="/login" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>');
+});
+
+app.use('/login', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
+    res.send('<h1>Hi You have Successfully Logged in!</h1>');
+    console.log('In the middleware!');
+});
+
+app.listen(3100);
